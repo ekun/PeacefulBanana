d593e03481cb4f2a8cd3ef1ef1f9706c557fadfa
Even <najtrox@gmail.com>
2012-10-19 13:56:33 +0200

    Added authentication plugin + controller

---
+++ grails-app/controllers/peacefulbanana/AuthenticationController.groovy
@@ -0,0 +1,20 @@
+package peacefulbanana
+
+
+class AuthenticationController {
+	
+	def authenticationService
+	
+//	
+//	def onlyLoggedInUsers = {
+//		if (!authenticationService.isLoggedIn(request)) {
+//			// Redirect or return forbidden
+//			response.sendError(403)
+//		}
+//		else {
+//			
+//		}
+//	}
+
+    def index() { }
+}
