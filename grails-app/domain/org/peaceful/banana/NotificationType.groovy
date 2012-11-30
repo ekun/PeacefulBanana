9f8ba1753dd4a75e2da045261ed84b2096ce9652
Marius Nedal Glittum <ekunamatata@gmail.com>
2012-11-30 11:27:42 +0100

    Added another type of notification #enum

--- grails-app/domain/org/peaceful/banana/NotificationType.groovy
+++ grails-app/domain/org/peaceful/banana/NotificationType.groovy
@@ -14,7 +14,8 @@ public enum NotificationType {
 
      */
     COLLABORATIV_SESSION("Collaborativ session"),
-    FIVE_MIN_REFLECTION("5 min reflection session")
+    FIVE_MIN_REFLECTION("5 min reflection session"),
+    OTHER("OTHER")
 
 
     String name
