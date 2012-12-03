9f40830697e393c66b48dea32ff0cfda3c09713c
Even <najtrox@gmail.com>
2012-12-03 14:21:07 +0100

    Added some fields

--- grails-app/domain/org/peaceful/banana/reflection/Summary.groovy
+++ grails-app/domain/org/peaceful/banana/reflection/Summary.groovy
@@ -8,11 +8,17 @@ class Summary {
     String contributions
     String improvements
     Date createdAt
+    boolean shared = false
 
     static belongsTo = [user: User]
 
     static constraints = {
         mood range: 1..100
+    }
+
+    static mapping = {
+        contributions type: "text"
+        improvements type: "text"
     }
 }
 
