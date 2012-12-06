5c6e5fc9d4439f9c5c3b7e0cd7d48fc3d13499d4
Even <najtrox@gmail.com>
2012-12-06 14:57:09 +0100

    Added Team domain class++. Need to specify what data we need out of the team, Members etc... #team #user

---
+++ grails-app/domain/org/peaceful/banana/Team.groovy
@@ -0,0 +1,9 @@
+package org.peaceful.banana
+
+class Team {
+
+    String name
+
+    static constraints = {
+    }
+}
--- grails-app/domain/org/peaceful/banana/User.groovy
+++ grails-app/domain/org/peaceful/banana/User.groovy
@@ -19,6 +19,7 @@ class User {
     String oAuthToken
     String gitLogin
 
+    static belongsTo = [team: Team]
 
 	static constraints = {
 		username blank: false, unique: true
